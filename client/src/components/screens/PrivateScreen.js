79e77c43a9da92d7cd9b379eaf765c0017d781d9
Megalonia <moosman863@gmail.com>
2022-03-07 23:19:40 -0600

    add css for ForgotPassword,Login,ResetPassword, add code for Login,Prive,Register

---
+++ client/src/components/screens/PrivateScreen.js
@@ -0,0 +1,18 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const PrivateScreen = ({history}) => {
+    const [error, setError] = useState("");
+    const [privateData, setPrivateData] = useState("");
+
+    useEffect(() => {
+        if(!localStorage.getItem("authToken")) {
+            history.push("/login")
+        }
+        const fetchPrivateData = async () =
+    }, [])
+    //return (
+    //)
+}
+
+export default PrivateScreen;
